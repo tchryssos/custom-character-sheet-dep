9025f5334e9da64281ef3c768c30d351978f72dc
Troy Chryssos <troychryssos@gmail.com>
2020-10-19 21:07:08 -0400

    finishing (for now) the sheet elements const, adding dice const

---
+++ src/constants/game/dice.ts
@@ -0,0 +1,26 @@
+export default [
+	{
+		label: 'd4',
+		value: 4,
+	},
+	{
+		label: 'd6',
+		value: 6,
+	},
+	{
+		label: 'd8',
+		value: 8,
+	},
+	{
+		label: 'd10',
+		value: 10,
+	},
+	{
+		label: 'd12',
+		value: 12,
+	},
+	{
+		label: 'd20',
+		value: 20,
+	},
+]
--- src/constants/game/sheetElements.ts
+++ src/constants/game/sheetElements.ts
@@ -1,35 +1,132 @@
+const metaTag = 'meta'
+const attributesTag = 'attributes'
+const skillsTag = 'skills'
+const combatTag = 'combat'
+
+// START - CHARACTER META - START
 const name = {
 	id: 'name',
 	label: 'Name',
-	tag: 'meta',
+	tag: metaTag,
 }
 
 const level = {
 	id: 'level',
 	label: 'Level',
-	tag: 'meta',
+	tag: metaTag,
 }
 
 const classes = {
 	id: 'classes',
 	label: 'Classes',
-	tag: 'meta',
+	tag: metaTag,
 }
 
 const race = {
 	id: 'race',
 	label: 'Race',
-	tag: 'meta',
+	tag: metaTag,
 }
 
 const alignment = {
 	id: 'alignment',
 	label: 'Alignment',
-	tag: 'meta',
+	tag: metaTag,
 }
 
+const profBonus = {
+	id: 'profBonus',
+	label: 'Proficiency Bonus',
+	tag: metaTag,
+}
+
+const description = {
+	id: 'description',
+	label: 'Description',
+	tag: metaTag,
+}
+
+// END - CHARACTER META - END
+
+// START - CHARACTER SKILLS & ATTRS - START
 const attributes = {
 	id: 'attributes',
 	label: 'Attributes',
-	tag: 'attributes',
+	tag: attributesTag,
 }
+
+const savingThrows = {
+	id: 'savingThrows',
+	label: 'Saving Throws',
+	tag: attributesTag,
+}
+
+const skills = {
+	id: 'skills',
+	label: 'Skills',
+	tag: skillsTag,
+}
+// END - CHARACTER SKILLS & ATTRS - END
+
+// START - COMBAT - START
+const armorClass = {
+	id: 'ac',
+	label: 'AC',
+	tag: combatTag,
+}
+
+const initiative = {
+	id: 'initiative',
+	label: 'Initiative',
+	tag: combatTag,
+}
+
+const speed = {
+	id: 'speed',
+	label: 'Speed',
+	tag: combatTag,
+}
+
+const hitPoints = {
+	id: 'hp',
+	label: 'Hit Points',
+	tag: combatTag,
+}
+
+const hitDice = {
+	id: 'hitDice',
+	label: 'Hit Dice',
+	tag: combatTag,
+}
+
+const deathSaves = {
+	id: 'deathSaves',
+	label: 'Death Saves',
+	tag: combatTag,
+}
+
+const attacks = {
+	id: 'attacks',
+	label: 'Attacks',
+	tag: combatTag,
+}
+
+const castingAbility = {
+	id: 'castingAbility',
+	label: 'Casting Ability',
+	tag: combatTag,
+}
+
+const castingSave = {
+	id: 'castingSave',
+	label: 'Spell Save DC',
+	tag: combatTag,
+}
+// END - COMBAT - END
+
+export const SHEET_ELEMENTS_ARRAY = [
+	name, level, classes, race, alignment,
+	profBonus, description, attributes, savingThrows, skills,
+	armorClass, initiative, speed, hitPoints, hitDice,
+	deathSaves, attacks, castingAbility, castingSave,
+]
