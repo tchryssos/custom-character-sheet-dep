c3c2ee09973b2e45c87ee58ca8b26b8183b96fbd
Troy Chryssos <troychryssos@gmail.com>
2020-10-19 20:52:14 -0400

    sheet elements file

---
+++ src/constants/game/sheetElements.ts
@@ -0,0 +1,35 @@
+const name = {
+	id: 'name',
+	label: 'Name',
+	tag: 'meta',
+}
+
+const level = {
+	id: 'level',
+	label: 'Level',
+	tag: 'meta',
+}
+
+const classes = {
+	id: 'classes',
+	label: 'Classes',
+	tag: 'meta',
+}
+
+const race = {
+	id: 'race',
+	label: 'Race',
+	tag: 'meta',
+}
+
+const alignment = {
+	id: 'alignment',
+	label: 'Alignment',
+	tag: 'meta',
+}
+
+const attributes = {
+	id: 'attributes',
+	label: 'Attributes',
+	tag: 'attributes',
+}
